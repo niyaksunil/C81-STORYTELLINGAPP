0ae87563030312bc888f6d8960821202e9ab909f
sunilkmathew <sunilkmathew35@gmail.com>
2021-12-03 20:12:19 +0530

    Created the StoryTelling App

---
+++ screens/CreateStory.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export default class CreateStory extends React.Component{
+    render(){
+        return(
+            <View style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Text>CreateStory</Text>
+            </View>
+        );
+    }
+}
+
+
